feca2a942461969070a00cc680fa5a68fbc03c96
Danylov <ol_danylov@ukr.net>
2022-07-02 15:37:00 +0300

    Using NavMesh Agent to moving Enemy to Player.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent navMeshAgent;
+    
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
