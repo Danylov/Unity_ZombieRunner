6977bedc4bb3b42a7345281b243c644ad0232d16
Danylov <ol_danylov@ukr.net>
2022-07-03 12:15:28 +0300

    Separate chase and attack Enemy to Player.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,18 +6,43 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float chaseRange;
+    [SerializeField] private float chaseRange = 10f;
     private NavMeshAgent navMeshAgent;
     private float distanceToTarget = Mathf.Infinity;
-    
-    void Start()
+    private bool isProvoked = false;
+
+    private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    private void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange) navMeshAgent.SetDestination(target.position);
+        if (isProvoked) EngageTarget();
+        else if (distanceToTarget <= chaseRange)  isProvoked = true;
+    }
+
+    private void EngageTarget()
+    {
+        if (navMeshAgent.stoppingDistance <= distanceToTarget) ChaseTarget();
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) AttackTarget();
+        
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+    
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has seeked and destroying " + target.name);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
