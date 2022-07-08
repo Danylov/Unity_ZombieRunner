395d40a48e6cb1d925884748601e297f6fa952ae
Danylov <ol_danylov@ukr.net>
2022-07-08 18:10:28 +0300

    Basic ammo functionality realization.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float damage = 30f;
     [SerializeField] private ParticleSystem  muzzleFlash;
     [SerializeField] private GameObject  hitEffect;
+    [SerializeField] private Ammo  ammoSlot;
     
     private void Update()
     {
@@ -17,8 +18,12 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
-        PlayMuzzleFlash();
-        ProcessRaycast();
+        if (0 < ammoSlot.GetCurrentAmmo()) 
+        {
+            PlayMuzzleFlash();
+            ProcessRaycast();
+            ammoSlot.ReduceCurrentAmmo();
+        }
     }
 
     void PlayMuzzleFlash()
