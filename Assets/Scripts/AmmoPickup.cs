98a1247556436611eef3b682e383ddf99b398ac5
Danylov <ol_danylov@ukr.net>
2022-07-10 13:59:26 +0300

    Ammo pickup.

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    [SerializeField] private int ammoAmount = 5;
+    [SerializeField] private AmmoType ammoType;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+        Destroy(gameObject);
+    }
+}
